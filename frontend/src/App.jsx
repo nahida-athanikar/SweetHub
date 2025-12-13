5a09b8dc3a205c88aebf0e52d4e32a6c157d3586
Nahida <nahidathanikar18@gmail.com>
2025-12-14 01:16:16 +0530

    test(app): add failing test for App layout with router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
+  
   return (
     <div className='app'>
       <Navbar />
